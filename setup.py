042dc4ee8bc15c4be391a7d9951101fbb5f22c9e
Will M. Farr <will.farr@ligo.org>
2021-07-09 21:58:21 -0400

    Will added to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(name='gwtones',
       version='0.1',
       description='Bayesian analysis of black hole ringdowns.',
-      author='Maximiliano Isi',
-      author_email='max.isi@ligo.org',
-      url='https://',
+      author='Maximiliano Isi, Will M. Farr',
+      author_email='max.isi@ligo.org, will.farr@stonybrook.edu;',
+      url='https://github.com/maxisi/gwtones',
       packages=['gwtones'],
       package_data={'gwtones': ['stan/*.stan']},
      )
