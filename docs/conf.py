13e477765f6b8762d15637e502428519c4cae848
max.isi <max.isi@ligo.org>
2022-03-29 23:07:40 -0400

    docs/conf.py test

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../ringdown/'))
+sys.path.insert(0, os.path.abspath('../ringdown/waveforms/'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
