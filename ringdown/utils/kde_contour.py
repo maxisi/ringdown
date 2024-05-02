c7160c49ae4b566aa6361976fba2d459c3b9a460
Max Isi <max.isi@ligo.org>
2024-05-02 19:33:22 -0400

    kdeplot updates

--- ringdown/utils/kde_contour.py
+++ ringdown/utils/kde_contour.py
@@ -1,9 +1,76 @@
-__all__ = ['Bounded_2d_kde', 'Bounded_1d_kde', 'kdeplot_2d_clevels']
+__all__ = ['Bounded_2d_kde', 'Bounded_1d_kde', 'kdeplot']
 
-from pylab import *
+import numpy as np
+from matplotlib import pyplot as plt
 import scipy.stats as ss
 import seaborn as sns
 import pandas as pd
+
+class Bounded_1d_kde(ss.gaussian_kde):
+    """ Represents a one-dimensional Gaussian kernel density estimator
+    for a probability distribution function that exists on a bounded
+    domain.
+
+    Authorship: Ben Farr, LIGO
+    """
+
+    def __init__(self, pts, x_min=None, x_max=None, *args, **kwargs):
+        """Initialize with the given bounds.  Either ``low`` or
+        ``high`` may be ``None`` if the bounds are one-sided.  Extra
+        parameters are passed to :class:`gaussian_kde`.
+
+        :param x_min: The lower x domain boundary.
+
+        :param x_max: The upper x domain boundary.
+        """
+        pts = np.atleast_1d(pts)
+
+        assert pts.ndim == 1, 'Bounded_1d_kde can only be one-dimensional'
+
+        super(Bounded_1d_kde, self).__init__(pts.T, *args, **kwargs)
+
+        self._x_min = x_min
+        self._x_max = x_max
+
+    @property
+    def x_min(self):
+        """The lower bound of the x domain."""
+        return self._x_min
+
+    @property
+    def x_max(self):
+        """The upper bound of the x domain."""
+        return self._x_max
+
+    def evaluate(self, pts):
+        """Return an estimate of the density evaluated at the given
+        points."""
+        pts = np.atleast_1d(pts)
+        assert pts.ndim == 1, 'points must be one-dimensional'
+
+        x = pts.T
+        pdf = super(Bounded_1d_kde, self).evaluate(pts.T)
+        if self.x_min is not None:
+            pdf += super(Bounded_1d_kde, self).evaluate(2*self.x_min - x)
+
+        if self.x_max is not None:
+            pdf += super(Bounded_1d_kde, self).evaluate(2*self.x_max - x)
+
+        return pdf
+
+    def __call__(self, pts):
+        pts = np.atleast_1d(pts)
+        out_of_bounds = np.zeros(pts.shape[0], dtype='bool')
+
+        if self.x_min is not None:
+            out_of_bounds[pts < self.x_min] = True
+        if self.x_max is not None:
+            out_of_bounds[pts > self.x_max] = True
+
+        results = self.evaluate(pts)
+        results[out_of_bounds] = 0.
+        return results
+
 
 # The following routine, Bounded_2d_kde, was copied from
 # https://git.ligo.org/publications/gw190412/gw190412-discovery/-/blob/851f91431b7c36e7ea66fa47e8516f2aef9d7daf/scripts/bounded_2d_kde.py
@@ -110,7 +177,7 @@ class Bounded_2d_kde(ss.gaussian_kde):
 # ############################################################################
 # PLOTTING
 
-def kdeplot_2d_clevels(xs, ys, levels=11, **kwargs):
+def kdeplot_2d_clevels(xs, ys, levels=10, fill=False, n_grid=128, **kws):
     """ Plot contours at specified credible levels.
 
     Arguments
@@ -136,22 +203,98 @@ def kdeplot_2d_clevels(xs, ys, levels=11, **kwargs):
     kwargs:
         additional arguments passed to plt.contour().
     """
+    try:
+        xs = xs.values.astype(float)
+        ys = ys.values.astype(float)
+    except AttributeError:
+        pass
+
+    if np.all(~np.isfinite(xs)) or np.all(~np.isfinite(ys)):
+        return None
     
-    if 'hue' in kwargs:
-        hues = kwargs.pop('hue')
+    # construct credible levels
+    try:
+        len(levels)
+        f = 1 - np.array(levels)
+    except TypeError:
+        f = np.linspace(0, 1, levels+1, endpoint=True)[1:-1]
+    if fill:
+        # f = np.concatenate([f, [1]])
+        kws['extend'] = 'max'
+    
+    # estimate bounded KDE from samples
+    if kws.get('auto_bound', False):
+        kws['x_min'] = min(xs)
+        kws['x_max'] = max(xs)
+        kws['y_min'] = min(ys)
+        kws['y_max'] = max(ys)
+    ks = ['x_min', 'x_max', 'y_min', 'y_max', 'bw_method', 'weights']
+    kde_kws = {k: kws.pop(k, None) for k in ks}
+    k = Bounded_2d_kde(np.column_stack((xs, ys)), **kde_kws)
+    
+    # evaluate KDE on all points
+    p = k(np.column_stack((xs, ys)))
+    
+    # the levels passed to the contour function have to be the
+    # values of the KDE at corresponding to the quantiles
+    # first get the order of the samples sorted by KDE value
+    # then find the values that correspond to the thresholds
+    i = np.argsort(p)
+    l = np.array([p[i[min(int(np.round(ff*len(p))), len(i)-1)]] for ff in f])
+    
+    # construct grid of x and y values based on the
+    # range of the samples
+    x_hi, x_lo = np.percentile(xs, 99), np.percentile(xs, 1)
+    y_hi, y_lo = np.percentile(ys, 99), np.percentile(ys, 1)
+    
+    Dx = x_hi - x_lo
+    Dy = y_hi - y_lo
+
+    x = np.linspace(x_lo-0.1*Dx, x_hi+0.1*Dx, n_grid)
+    y = np.linspace(y_lo-0.1*Dy, y_hi+0.1*Dy, n_grid)
+
+    # construct grids and evaluate KDE
+    XS, YS = np.meshgrid(x, y, indexing='ij')
+    ZS = k(np.column_stack((XS.flatten(), YS.flatten()))).reshape(XS.shape)
+
+    ax = kws.pop('ax', plt.gca())
+    p = kws.get('cmap', kws.pop('palette', None))
+    if p is not None:
+        kws['colors'] = None
+        kws['cmap'] = p
+    else:
+        kws['colors'] = kws.get('colors', 
+                                [kws.pop('color', kws.pop('c', None)),])
+    if fill:
+        ax.contourf(XS, YS, ZS, levels=l, **kws)
+    else:
+        ax.contour(XS, YS, ZS, levels=l, **kws)
+
+
+def kdeplot(xs, ys=None, **kws):
+    
+    if np.all(~np.isfinite(xs)):
+        return None
+    
+    if 'hue' in kws:
+        hues = kws.pop('hue')
         hues_unique = sorted(hues.unique())
         n_hues = len(hues_unique)
         
-        if 'palette' in kwargs:
-            palette = sns.color_palette(kwargs.pop('palette'), n_colors=n_hues)
+        if 'palette' in kws:
+            palette = sns.color_palette(kws.pop('palette'), n_colors=n_hues)
         else:
             # attempt to use seaborn's logic for determining the color palette
             # (this will fail if seaborn changes it's internal API)
             try:
                 from seaborn.distributions import _DistributionPlotter
                 df = pd.DataFrame(dict(x=xs, y=ys, hue=hues))
-                dp = _DistributionPlotter(data=df, 
-                    variables=dict(x='x', y='y', hue='hue'))
+                if ys is None:
+                    df = df.drop(columns='y')
+                    v = dict(x='x', hue='hue')
+                else:
+                    v = dict(x='x', y='y', hue='hue')
+                dp = _DistributionPlotter(data=df, variables=v)
                 palette = [dp._hue_map(l) for l in dp._hue_map.levels]
             except Exception:
                 if pd.api.types.is_numeric_dtype(hues_unique):
@@ -160,118 +303,36 @@ def kdeplot_2d_clevels(xs, ys, levels=11, **kwargs):
                 else:
                     # Categorical: Use a qualitative palette
                     palette = sns.color_palette("deep", n_colors=n_hues)
-    
+
         for hue, color in zip(hues_unique, palette):
             xs_hue = xs[hues == hue]
-            ys_hue = ys[hues == hue]
-            kdeplot_2d_clevels(xs_hue, ys_hue, levels=levels, **kwargs, color=color)
+            if ys is None:
+                ys_hue = None
+            else:
+                ys_hue = ys[hues == hue]
+            kdeplot(xs_hue, ys_hue, **kws, color=color)
             plt.plot([], [], c=color, label=hue)
         plt.legend();
         return
     
-    try:
-        xs = xs.values.astype(float)
-        ys = ys.values.astype(float)
-    except AttributeError:
-        pass
+    if ys is not None:
+        return kdeplot_2d_clevels(xs, ys, **kws)
     
-    if all(~isfinite(xs)) or all(~isfinite(ys)):
-        return None
-    try:
-        len(levels)
-        f = 1 - np.array(levels)
-    except TypeError:
-        f = linspace(0, 1, levels+2)[1:-1]
-    if kwargs.get('auto_bound', False):
-        kwargs['x_min'] = min(xs)
-        kwargs['x_max'] = max(xs)
-        kwargs['y_min'] = min(ys)
-        kwargs['y_max'] = max(ys)
-    kde_kws = {k: kwargs.pop(k, None) for k in ['x_min', 'x_max', 'y_min', 'y_max']}
-        
-    k = Bounded_2d_kde(np.column_stack((xs, ys)), **kde_kws)
-    size = max(10*(len(f)+2), 500)
-    c = np.random.choice(len(xs), size=size)
-    p = k(np.column_stack((xs[c], ys[c])))
-    i = argsort(p)
-    l = array([p[i[int(round(ff*len(i)))]] for ff in f])
-
-    Dx = np.percentile(xs, 99) - np.percentile(xs, 1)
-    Dy = np.percentile(ys, 99) - np.percentile(ys, 1)
-
-    x = linspace(np.percentile(xs, 1)-0.1*Dx, np.percentile(xs, 99)+0.1*Dx, 128)
-    y = linspace(np.percentile(ys, 1)-0.1*Dy, np.percentile(ys, 99)+0.1*Dy, 128)
-
-    XS, YS = meshgrid(x, y, indexing='ij')
-    ZS = k(np.column_stack((XS.flatten(), YS.flatten()))).reshape(XS.shape)
-
-    ax = kwargs.pop('ax', gca())
-    kwargs['colors'] = kwargs.get('colors', [kwargs.pop('color', None),])
-
-    ax.contour(XS, YS, ZS, levels=l, **kwargs)
-
-
-class Bounded_1d_kde(ss.gaussian_kde):
-    """ Represents a one-dimensional Gaussian kernel density estimator
-    for a probability distribution function that exists on a bounded
-    domain.
-
-    Authorship: Ben Farr, LIGO
-    """
-
-    def __init__(self, pts, x_min=None, x_max=None, *args, **kwargs):
-        """Initialize with the given bounds.  Either ``low`` or
-        ``high`` may be ``None`` if the bounds are one-sided.  Extra
-        parameters are passed to :class:`gaussian_kde`.
-
-        :param x_min: The lower x domain boundary.
-
-        :param x_max: The upper x domain boundary.
-        """
-        pts = np.atleast_1d(pts)
-
-        assert pts.ndim == 1, 'Bounded_1d_kde can only be one-dimensional'
-
-        super(Bounded_1d_kde, self).__init__(pts.T, *args, **kwargs)
-
-        self._x_min = x_min
-        self._x_max = x_max
-
-    @property
-    def x_min(self):
-        """The lower bound of the x domain."""
-        return self._x_min
-
-    @property
-    def x_max(self):
-        """The upper bound of the x domain."""
-        return self._x_max
-
-    def evaluate(self, pts):
-        """Return an estimate of the density evaluated at the given
-        points."""
-        pts = np.atleast_1d(pts)
-        assert pts.ndim == 1, 'points must be one-dimensional'
-
-        x = pts.T
-        pdf = super(Bounded_1d_kde, self).evaluate(pts.T)
-        if self.x_min is not None:
-            pdf += super(Bounded_1d_kde, self).evaluate(2*self.x_min - x)
-
-        if self.x_max is not None:
-            pdf += super(Bounded_1d_kde, self).evaluate(2*self.x_max - x)
-
-        return pdf
-
-    def __call__(self, pts):
-        pts = np.atleast_1d(pts)
-        out_of_bounds = np.zeros(pts.shape[0], dtype='bool')
-
-        if self.x_min is not None:
-            out_of_bounds[pts < self.x_min] = True
-        if self.x_max is not None:
-            out_of_bounds[pts > self.x_max] = True
-
-        results = self.evaluate(pts)
-        results[out_of_bounds] = 0.
-        return results
+    if kws.pop('auto_bound', False):
+        kws['x_min'] = min(xs)
+        kws['x_max'] = max(xs)
+    kde_kws = {k: kws.pop(k, None) for k in 
+               ['x_min', 'x_max', 'bw_method', 'weights']}
+    k = Bounded_1d_kde(xs, **kde_kws)
+    
+    x_hi, x_lo = np.percentile(xs, 99), np.percentile(xs, 1)
+    Dx = x_hi - x_lo
+    xgrid = np.linspace(x_lo-0.1*Dx, x_hi+0.1*Dx, kws.pop('n_grid', 128))
+    ygrid = k(xgrid)
+    ax = kws.pop('ax', plt.gca())
+    ax.plot(xgrid, ygrid, **kws)
+    
+    if kws.get('fill', False):
+        alpha = kws.get('alpha', 0.5)
+        ax.fill_between(xgrid, 0, ygrid, alpha=alpha, **kws)
+    
