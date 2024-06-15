f0df0c4b7de29bc90f58e0bda2c0d2ff7b591c35
Max Isi <max.isi@ligo.org>
2024-06-14 21:55:42 -0400

    PEP for tests

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -18,7 +18,8 @@ STR_INDICES_SHORT = [
     '320',
 ]
 INT_INDICES = [1, 2, 3]
-ALL_INDICES = TUPLE_INDICES + STR_INDICES_LONG + STR_INDICES_SHORT + INT_INDICES
+ALL_INDICES = TUPLE_INDICES + STR_INDICES_LONG +\
+              STR_INDICES_SHORT + INT_INDICES
 
 
 @pytest.mark.parametrize("index", ALL_INDICES)
@@ -38,8 +39,8 @@ def _test_index_base(mode):
     assert isinstance(mode.get_coordinate(), (bytes, int))
     assert isinstance(mode.is_prograde, bool)
     assert isinstance(mode.as_dict(), dict)
-    
-    
+
+
 def _test_harmonic(mode, ref_index):
     assert isinstance(mode, ringdown.indexing.HarmonicIndex)
     assert mode.p == ref_index[0]
@@ -49,8 +50,8 @@ def _test_harmonic(mode, ref_index):
     assert mode.n == ref_index[4]
     assert isinstance(mode.get_kerr_mode(), ringdown.qnms.KerrMode)
     _test_index_base(mode)
-    
-    
+
+
 @pytest.mark.parametrize("index", INT_INDICES)
 def test_construct_generic(index):
     mode = ringdown.indexing.ModeIndex.construct(index)
@@ -61,14 +62,15 @@ def test_construct_generic(index):
 
 @pytest.mark.parametrize(["index", "ref_index"],
                          zip(TUPLE_INDICES + STR_INDICES_LONG,
-                          TUPLE_INDICES*2))
+                             TUPLE_INDICES*2))
 def test_construct_harmonic(index, ref_index):
     mode = ringdown.indexing.ModeIndex.construct(index)
     _test_harmonic(mode, ref_index)
-    
-    
+
+
 @pytest.mark.parametrize(["index", "ref_index"],
-                         zip(STR_INDICES_LONG + STR_INDICES_SHORT, TUPLE_INDICES))
+                         zip(STR_INDICES_LONG + STR_INDICES_SHORT,
+                             TUPLE_INDICES))
 def test_construct_harmonic_from_string(index, ref_index):
     mode = ringdown.indexing.HarmonicIndex.from_string(index)
     _test_harmonic(mode, ref_index)
@@ -80,8 +82,8 @@ def test_bytestrings(index):
     bs = mode.to_bytestring()
     mode_from_bs = ringdown.indexing.HarmonicIndex.from_bytestring(bs)
     assert mode == mode_from_bs
-    
-    
+
+
 @pytest.mark.parametrize("indices", [
     TUPLE_INDICES,
     STR_INDICES_LONG,
@@ -91,4 +93,5 @@ def test_bytestrings(index):
 def test_mode_index_list(indices):
     mil = ringdown.indexing.ModeIndexList(indices)
     assert len(mil.indices) == len(indices)
-    assert all(isinstance(m, ringdown.indexing.ModeIndex) for m in mil.indices)
+    assert all(isinstance(m, ringdown.indexing.ModeIndex)
+               for m in mil.indices)
--- tests/test_qnms.py
+++ tests/test_qnms.py
@@ -11,8 +11,10 @@ FTAU_REF = {
     (1, -2, 3, 2, 0): (350.440511991165, 0.004095329813033091)
 }
 
+
 def test_T_MSUN():
     assert ringdown.qnms.T_MSUN == T_MSUN
+
 
 @pytest.mark.parametrize("index, f_tau_ref", FTAU_REF.items())
 def test_get_ftau(index, f_tau_ref):
@@ -22,24 +24,25 @@ def test_get_ftau(index, f_tau_ref):
 
 
 class TestKerrMode:
-    
+
     def setup_method(self, method):
         self.mass = M_REF
         self.spin = C_REF
         self.kerr_modes = {m: ringdown.qnms.KerrMode(*m)
                            for m in FTAU_REF.keys()}
-    
+
     def test_ftau(self):
         for (p, s, l, m, n), mode in self.kerr_modes.items():
             ref = ringdown.qnms.get_ftau(self.mass, self.spin, n=n, l=l, m=p*m)
             assert mode.ftau(self.spin, self.mass) == ref
-            
+
     def test_fgamma(self):
         for (p, s, l, m, n), mode in self.kerr_modes.items():
-            f_ref, t_ref = ringdown.qnms.get_ftau(self.mass, self.spin, n=n, l=l, m=p*m)
+            f_ref, t_ref = ringdown.qnms.get_ftau(self.mass, self.spin, n=n,
+                                                  l=l, m=p*m)
             f, g = mode.fgamma(self.spin, self.mass)
             assert (f, g) == pytest.approx((f_ref, 1/t_ref), abs=1e-12)
-            
+
     @pytest.mark.slow
     def test_ftau_approx(self):
         for (p, s, l, m, n), mode in self.kerr_modes.items():
