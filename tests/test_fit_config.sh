6b01febb3216cd6496697a5c59f7c15f21a95827
Max Isi <max.isi@ligo.org>
2024-10-10 10:10:54 -0400

    test fit: do not download data if already present

--- tests/test_fit_config.sh
+++ tests/test_fit_config.sh
@@ -2,8 +2,8 @@
 
 mkdir -p data
 cd data
-wget https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/H-H1_LOSC_4_V2-1126259446-32.hdf5
-wget https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/L-L1_LOSC_4_V2-1126259446-32.hdf5
+wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/H-H1_LOSC_4_V2-1126259446-32.hdf5
+wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/L-L1_LOSC_4_V2-1126259446-32.hdf5
 
 cd ..
 
