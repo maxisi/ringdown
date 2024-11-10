2dccf5b471327ba42c0eb0d1084497a345fe7a9f
Max Isi <max.isi@ligo.org>
2024-11-10 18:08:50 -0500

    test pipe config

---
+++ tests/test_pipe_config.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+mkdir -p data
+cd data
+wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/H-H1_LOSC_4_V2-1126259446-32.hdf5
+wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/L-L1_LOSC_4_V2-1126259446-32.hdf5
+
+cd ..
+
+ringdown_pipe etc/ringdown_pipe_config_example.ini
