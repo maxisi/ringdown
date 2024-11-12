72b0587c2b71184ca6af76f2a2332c003abe0d1d
Max Isi <max.isi@ligo.org>
2024-11-12 13:22:32 -0500

    target collection from config wip

--- ringdown/target.py
+++ ringdown/target.py
@@ -628,33 +628,42 @@ class TargetCollection(utils.MultiIndexCollection):
         config = utils.load_config(config_input)
 
         # Check if we are to get reference values from IMR result
-        if config.getboolean(t0_sect, 'reference_imr', fallback=False):
+        reference_imr = any([
+            config.get(t0_sect, k, fallback='').lower() == 'imr'
+            for k in [MREF_KEY, TREF_KEY]])
+        if reference_imr:
             if not config.has_section('imr'):
                 raise ValueError("IMR reference requested but no IMR section "
                                  "in config file")
             # get the IMR result
             from .imr import IMRResult
-            imr = {k: utils.try_parse(v) for k, v in config['imr'].items()}
-            imr_result = IMRResult.construct(**imr)
-            imr_target = imr_result.get_best_peak_target().as_dict()
+            imr = {k: utils.try_parse(v) for k, v in config['imr'].items()
+                   if k not in ['initialize_fit', 'psds']}
+            path = imr.pop('path')
+            imr_result = IMRResult.construct(path, **imr)
+            # WIP
+            imr_duration = imr_result.estimate_ringdown_duration(cache=True)
+            imr_target = imr_result.get_best_peak_target(
+                duration=imr_duration).settings
             # set the reference mass and time
             if MREF_KEY in config[t0_sect]:
-                if config[t0_sect][MREF_KEY].lower() == 'auto':
+                if config[t0_sect][MREF_KEY].lower() == 'imr':
                     logging.info("using IMR result for reference mass")
-                else:
-                    logging.warning("overwriting reference mass")
-                config[t0_sect][MREF_KEY] = \
-                    imr_result.remnant_mass_scale_reference
+                    config[t0_sect][MREF_KEY] = \
+                        str(imr_result.remnant_mass_scale_reference)
             else:
                 logging.warning("no reference mass requested")
 
-            if config.get(t0_sect, T0_KEYS['ref'], fallback='auto') == 'auto':
-                logging.info("using IMR result for reference time")
+            if TREF_KEY in config[t0_sect]:
+                if config[t0_sect][TREF_KEY].lower() == 'imr':
+                    logging.info("using IMR result for reference time")
+                    config[t0_sect][TREF_KEY] = str(imr_target.pop('t0'))
             else:
-                logging.warning("overwriting reference time")
-            config[t0_sect][T0_KEYS['ref']] = imr_target.pop('t0')
-
-            config[sky_sect].update(imr_target)
+                logging.warning("no reference time requested")
+            if sky_sect in config:
+                config[sky_sect].update(imr_target)
+            else:
+                config[sky_sect] = imr_target
 
         # Look for a reference mass, to be used when stepping in time
         m_ref = config.getfloat(t0_sect, MREF_KEY, fallback=None)
