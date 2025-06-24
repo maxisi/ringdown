06eab18a0aa8a99ab6d1f5f3805a50763b717669
Max Isi <max.isi@ligo.org>
2025-06-24 15:48:50 -0400

    target kws

--- ringdown/target.py
+++ ringdown/target.py
@@ -231,6 +231,7 @@ class SkyTarget(Target):
         psi: float,
         reference_ifo: str | None = None,
         duration: float = 0.0,
+        **kws,
     ):
         """Create a sky location from a reference time, either a specific
         detector or geocenter.
@@ -262,6 +263,8 @@ class SkyTarget(Target):
             tgps = lal.LIGOTimeGPS(t0)
             dt = lal.TimeDelayFromEarthCenter(det.location, ra, dec, tgps)
             tgeo = t0 - dt
+        if kws:
+            logger.info(f"ignoring {kws}")
         return cls(lal.LIGOTimeGPS(tgeo), ra, dec, psi, duration)
 
     @property
