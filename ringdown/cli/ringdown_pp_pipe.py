8f70c5f082b68e3fe22834b6f1b64b0a68c04c2f
Max Isi <max.isi@ligo.org>
2025-06-26 01:05:09 -0400

    cli revert

--- ringdown/cli/ringdown_pp_pipe.py
+++ ringdown/cli/ringdown_pp_pipe.py
@@ -376,24 +376,20 @@ def main(args=None):
             "",
             f"cd {outdir}",
             "# prior job",
-            f'priorid=$(get_id "$(sbatch -p genx -c 4 -t 0-1 {
-                PATHS["prior_exe"]})")',
+            f'priorid=$(get_id "$(sbatch -p genx -c 4 -t 0-1 {PATHS["prior_exe"]})")',
             "",
             "# pp jobs",
-            f'ppid=$(get_id "$({command} --dependency=afterok:$priorid -p cca -n {
-                NTASK} -c {NDEVICE} disBatch {PATHS["run_task"]})")',
+            f'ppid=$(get_id "$({command} --dependency=afterok:$priorid -p cca -n {NTASK} -c {NDEVICE} disBatch {PATHS["run_task"]})")',
             "",
             "# collect job",
-            f"{command} --dependency=afterok:$ppid -p genx -c 4 -t 0-1 {
-                PATHS['collect_exe']}",
+            f"{command} --dependency=afterok:$ppid -p genx -c 4 -t 0-1 {PATHS['collect_exe']}",
             "cd -",
         ]
     else:
         # these options are set to match the GPU nodes at the Flatiron Institute
         # see https://wiki.flatironinstitute.org/SCC/Software/UsingTheGPUNodes
         NCPU = 16
-        prior_command = f"sbatch -p gpu --gpus-per-task=1 --cpus-per-task={
-            NCPU} {PATHS['prior_exe']}"
+        prior_command = f"sbatch -p gpu --gpus-per-task=1 --cpus-per-task={NCPU} {PATHS['prior_exe']}"
         pp_command = (
             f"{command} --dependency=afterok:$priorid -p gpu -n {NTASK} "
             f"--gpus-per-task={NDEVICE} "
@@ -422,8 +418,7 @@ def main(args=None):
             f'ppid=$(get_id "$({pp_command})")',
             "",
             "# collect job",
-            f"{command} --dependency=afterok:$ppid -p genx -c 4 -t 0-1 {
-                PATHS['collect_exe']}",
+            f"{command} --dependency=afterok:$ppid -p genx -c 4 -t 0-1 {PATHS['collect_exe']}",
             "cd -",
         ]
 
