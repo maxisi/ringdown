9117498546e8ed95353afc00aa5648f8045ecac6
Max Isi <max.isi@ligo.org>
2026-08-28 13:20:06 -0400

    replaced wget with curl for portability

--- tests/test_fit_config.sh
+++ tests/test_fit_config.sh
@@ -2,8 +2,15 @@
 
 mkdir -p data
 cd data
-wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/H-H1_LOSC_4_V2-1126259446-32.hdf5
-wget -nc https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/L-L1_LOSC_4_V2-1126259446-32.hdf5
+for url in \
+    https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/H-H1_LOSC_4_V2-1126259446-32.hdf5 \
+    https://www.gw-openscience.org/eventapi/html/O1_O2-Preliminary/GW150914/v2/L-L1_LOSC_4_V2-1126259446-32.hdf5
+do
+    f=$(basename "$url")
+    if [ ! -f "$f" ]; then
+        curl -fsSL -o "$f" "$url"
+    fi
+done
 
 cd ..
 
